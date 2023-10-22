215837c0dee82d3315e9eb8caca95a5e3231f8c4
Weab_ <135516355+Weab69@users.noreply.github.com>
2023-10-22 23:21:53 +0300

    Time: 149 ms (65.80%), Space: 17.9 MB (43.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        co={}
-        res,maxCo=0,0
+        res,co=0,0
         
         for n in nums:
-            co[n]=1+co.get(n,0)
-            res= n if co[n]>maxCo else res
-            maxCo=max(co[n],maxCo)
-        return res
-        
+            if co==0:
+                res=n
+            co+=(1 if n==res else -1)
+        return res     
+            
+         
